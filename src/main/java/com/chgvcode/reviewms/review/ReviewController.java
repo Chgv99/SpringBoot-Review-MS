71d4d147b3c0f69daa6a465653a55fc39418dd07
Christian García <christiangv99@gmail.com>
2024-06-24 13:02:43 +0100

    Added average rating endpoint

--- src/main/java/com/chgvcode/reviewms/review/ReviewController.java
+++ src/main/java/com/chgvcode/reviewms/review/ReviewController.java
@@ -53,4 +53,10 @@ public class ReviewController {
         }
         return new ResponseEntity<>("Review not deleted", HttpStatus.NOT_FOUND);
     }
+
+    @GetMapping("/averageRating")
+    public Double getAverageReview(@RequestParam Long companyId){
+        List<Review> reviewList = reviewService.findAll(companyId);
+        return reviewList.stream().mapToDouble(Review::getRating).average().orElse(0.0);
+    }
 }
